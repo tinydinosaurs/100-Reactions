6902472273f4e8ae39686d33e3df462b1a46f9fa
Dana <dana.lachman@gmail.com>
2017-01-11 17:00:08 -0600

    home page is all fancy styled. :rainbow:

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>why is everyone yelling?</h1>
-        <div className="project">
-          <Link to='/login'>login form</Link>
-        </div>
-        <div className="project">
-          <Link to='/checkout'>checkout form</Link>
-        </div>
-        <div className="project">
-          <Link to='/netflix'>netflix landing page</Link>
+        <h1 className="home__h1">why is everyone yelling?</h1>
+        <div className="projects">
+          <div className="project project__color-1">
+            <Link to='/login'>login form</Link>
+          </div>
+          <div className="project project__color-2">
+            <Link to='/checkout'>checkout form</Link>
+          </div>
+          <div className="project project__color-3">
+            <Link to='/netflix'>netflix landing page</Link>
+          </div>
+          <div className="project project__color-4">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-5">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-6">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-7">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-8">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-9">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
+          <div className="project project__color-1">
+            {/*<Link to='/netflix'>netflix landing page</Link>*/}
+          </div>
         </div>
       </div>
     );
